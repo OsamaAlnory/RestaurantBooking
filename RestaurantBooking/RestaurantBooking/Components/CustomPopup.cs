3588f7faddcc5897480ee19bb5630e41a8c84925
LEARNET\osama.alnouri1 <m.ahmad1999@hotmail.com>
2019-11-12 10:46:10 +0100

    new

---
+++ RestaurantBooking/RestaurantBooking/Components/CustomPopup.cs
@@ -0,0 +1,14 @@
+﻿using Rg.Plugins.Popup.Animations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace RestaurantBooking.Components
+{
+    public interface CustomPopup
+    {
+        void ChangeFrame(Frame frame);
+        void ChangeAnimation(ScaleAnimation animation);
+    }
+}
