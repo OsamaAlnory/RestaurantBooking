5401093badaf66295c86ee303c6ac84f8869976c
ibrahim.alibrahimal <ibrahim.alibrahimal@373Q5H2>
2020-01-13 14:52:36 +0100

    - LoginPage - DisplayPage - etc.

--- RestaurantBooking/RestaurantBooking/Database/User.cs
+++ RestaurantBooking/RestaurantBooking/Database/User.cs
@@ -6,10 +6,25 @@ namespace RestaurantBooking.Database
 {
     public class User
     {
+        public static List<User> users = new List<User>();
 
         public string RestID { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
         public int UType { get; set; }
+
+
+        public static User GetUserByName(string name)
+        {
+            for(int x = 0; x < users.Count; x++)
+            {
+                if(users[x].Username == name)
+                {
+                    return users[x];
+                }
+            }
+            return null;
+        }
 
     }
 }
---
+++ RestaurantBooking/RestaurantBooking/Pages/LoginPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using RestaurantBooking.Components;
+using RestaurantBooking.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RestaurantBooking.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string u1 = uname.Text, p1 = password.Text;
+            if(!(string.IsNullOrEmpty(u1) || string.IsNullOrEmpty(p1)))
+            {
+                User user = User.GetUserByName(u1);
+                if(user != null && user.Password == p1)
+                {
+                    animation.IsVisible = true;
+                    animation.Play();
+                    Device.StartTimer(TimeSpan.FromSeconds(2), () =>
+                    {
+                        if(user.UType == 0)
+                        {
+                            Navigation.PushAsync(new RestaurantPage());
+                        } else if(user.UType == 1)
+                        {
+                            Navigation.PushAsync(new DisplayPage());
+                        }
+                        ClosePage();
+                        return false;
+                    });
+                } else
+                {
+                    new Popup(new ErrorMessage("Username or password is incorrect!"), this).Show();
+                }
+            } else
+            {
+                new Popup(new ErrorMessage("Enter username and password."), this).Show();
+            }
+        }
+
+        private void ClosePage()
+        {
+            
+        }
+
+
+    }
+}
