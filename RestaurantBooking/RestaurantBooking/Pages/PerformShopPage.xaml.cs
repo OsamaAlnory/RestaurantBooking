90562f0dbd6cf567f503c9656031dbd19d64b4bd
ibrahim.alibrahimal <ibrahim.alibrahimal@373Q5H2>
2020-01-15 16:37:43 +0100

    ...

---
+++ RestaurantBooking/RestaurantBooking/Pages/PerformShopPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RestaurantBooking.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PerformShopPage : ContentPage
+    {
+        public PerformShopPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
